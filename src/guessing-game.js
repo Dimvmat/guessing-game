786889c8a95160b86bf580ffd213cadca53a235e
dimvmat <102551294+Dimvmat@users.noreply.github.com>
2023-10-26 00:09:23 +0300

    feat: final edits

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+       this.min = min;
+       this.max = max;
+       this.n = 0;
     }
 
     guess() {
-
+       this.n = Math.ceil((this.min + this.max) / 2);
+       return this.n
     }
 
     lower() {
-
+       this.max = this.n;
     }
 
     greater() {
-
+       this.min = this.n;
     }
 }
 
